dfcf375bc410c4c96354c6ee2591b319f5a1b653
GabytaDev <gabyta.loyola@gmail.com>
2022-01-22 13:49:39 -0300

    agrego estilos a texto card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,13 @@ import "./_Card.scss";
 const Card = ({title,type,price,img,isAvailable, onSale})=>{
     return (
         <div className={`card ${isAvailable === false && "not-available"}`}>
-            <h2 className="text">
-                {title} {onSale === true && <span>On sale</span>}</h2>
-            <p className="text">{type}</p>
-            <p className="text">{price}</p>
             <div className="content-img">
             <img src={img}></img>
-            
             </div>
+            <h2 className="text">
+                {title} {onSale === true && <span>On sale</span>}</h2>
+            <p className="category">{type}</p>
+            <p className="text">$ {price}</p>
             
         </div>
     )
